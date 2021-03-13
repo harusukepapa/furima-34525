d88470192bf26a7c277ab5015c4cd9e99499a047
harusukepapa <fumikazu0522@gmail.com>
2021-03-13 11:35:31 +0900

    修正 テストコード、バリデーション、

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }, presence: true
-  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }, presence: true
-  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }, presence: true
-  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }, presence: true
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday
+  end
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
   
