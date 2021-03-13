29e83340a84d4b3a7b0fdd34a332bb5a0ffe7a01
harusukepapa <fumikazu0522@gmail.com>
2021-03-13 15:28:57 +0900

    モデルとテーブルの作成

---
+++ app/models/product.rb
@@ -0,0 +1,17 @@
+class Product < ApplicationRecord
+
+  with_options presence: true do
+    validates :product
+    validates :product_description
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :shipping_area_id
+    validates :shipping_time_id
+    validates :price
+  end
+
+  belongs_to :user
+  has_one :purchases_record
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_031319) do
+ActiveRecord::Schema.define(version: 2021_03_13_060059) do
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "product", null: false
+    t.text "product_description", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "shipping_fee_id", null: false
+    t.integer "shipping_area_id", null: false
+    t.integer "shipping_time_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_03_09_031319) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "users"
 end
