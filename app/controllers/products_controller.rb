6c47ef610cd4c7cdac309e9ffa4a26ad9c33f70f
harusukepapa <fumikazu0522@gmail.com>
2021-03-17 15:26:47 +0900

    商品情報編集機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,14 +22,27 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+    unless @product.user_id == current_user.id
+      redirect_to products_path
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+       redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
 
   private
 
   def product_params
     params.require(:product).permit(:image, :product, :product_description, :category_id, :condition_id, :shipping_fee_id, :shipping_area_id, :shipping_time_id, :price ).merge(user_id: current_user.id)
   end
-
-  
-
 
 end
